4b4ef3d7b6746e0c1deb863ccdb14b71a0354660
Chui Tey <teyc@cognoware.com>
2013-12-13 14:00:31 +1000

    Added support code for locating initial frame.

---
+++ LinqExtensions.cs
@@ -0,0 +1,51 @@
+﻿namespace Caliburn.Micro
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    internal static class LinqExtensions
+    {
+        public static IEnumerable<UIElement> Descendants(this UIElement e)
+        {
+            if (e is Panel)
+            {
+                Panel p = e as Panel;
+                foreach (UIElement c in p.Children)
+                {
+                    yield return c;
+                }
+
+                foreach (UIElement c in p.Children.SelectMany(d => d.Descendants()))
+                {
+                    yield return c;
+                }
+            }
+            else if (e is UserControl)
+            {
+                UserControl u = e as UserControl;
+                yield return u;
+
+                foreach (var c in (u.Content as UIElement).Descendants())
+                {
+                    yield return c;
+                }
+            }
+            else if (e is Border)
+            {
+                yield return e;
+                yield return (e as Border).Child as UIElement;
+            }
+            else if (e is ContentControl)
+            {
+                yield return e;
+                yield return (e as ContentControl).Content as UIElement;
+            }
+            else
+            {
+                yield return e;
+            }
+        }
+    }
+}
--- NavigationExtensions.cs
+++ NavigationExtensions.cs
@@ -1,4 +1,11 @@
 ﻿namespace Caliburn.Micro {
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows;
+    using System.Windows.Controls;
+    
     /// <summary>
     /// Extension methods related to navigation.
     /// </summary>
@@ -10,7 +17,40 @@
         /// <param name="navigationService">The navigation service.</param>
         /// <returns>The builder.</returns>
         public static UriBuilder<TViewModel> UriFor<TViewModel>(this INavigationService navigationService) {
+            if (navigationService == null)
+            {
+                //navigationService = IoC.Get<INavigationService>(null);
+            }
+            if (navigationService == null)
+            {
+                throw new Exception("NavigationService must not be null");
+            }
+            
             return new UriBuilder<TViewModel>().AttachTo(navigationService);
         }
+
+        /// <summary>
+        /// Finds a Frame control in a UserControl
+        /// </summary>
+        /// <param name="fe">The parent element</param>
+        /// <param name="name">Optional name of the frame if known</param>
+        /// <returns></returns>
+        public static Frame FindFrame(this UIElement fe, string name = null)
+        {
+            if (name != null)
+            {
+                return (from c in fe.Descendants()
+                        where name.Equals(c.GetValue(FrameworkElement.NameProperty))
+                             && c is Frame
+                        select c).FirstOrDefault() as Frame;
+            }
+            else
+            {
+                return (from c in fe.Descendants()
+                        where c is Frame
+                        select c).FirstOrDefault() as Frame;
+            }
+        }
+
     }
 }
